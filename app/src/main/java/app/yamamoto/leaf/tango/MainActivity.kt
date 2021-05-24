a20dba57ef2688be9515935461bc997c04509273
leaf-yamamoto <sanaeyamamoto@icloud.com>
2021-05-24 17:37:36 +0900

    単語アプリ（スキルチェック）の完成

--- app/src/main/java/app/yamamoto/leaf/tango/MainActivity.kt
+++ app/src/main/java/app/yamamoto/leaf/tango/MainActivity.kt
@@ -10,54 +10,38 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val apple = Word(R.drawable.apple_icon, "りんご")
+    val banana = Word(R.drawable.banana_icon, "バナナ")
+    val orange = Word(R.drawable.orange_icon, "オレンジ")
+    val strawberry = Word(R.drawable.strawberry_icon, "いちご")
+
+
     override fun onCreate (savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val apple = Word(R.drawable.apple_icon, "りんご")
-        val banana = Word(R.drawable.banana_icon, "バナナ")
-        val orange = Word(R.drawable.orange_icon, "オレンジ")
-        val strawberry = Word(R.drawable.strawberry_icon, "いちご")
 //りんご
+        tango (word = apple)
+//バナナ
+        tango (word = banana)
+//オレンジ
+        tango (word = orange)
+//いちご
+        tango (word = strawberry)
+    }
+
+    fun tango (word: Word = apple) {
         val nameTextView = TextView(this)
-        nameTextView.text = apple.name
+        nameTextView.text = word.name
+
         val layout = LinearLayout(this)
         layout.orientation = LinearLayout.HORIZONTAL
+
         val imageView = ImageView(this)
-        imageView.setImageResource(apple.resId)
+        word.resId?.let { imageView.setImageResource(it) }
         layout.addView(imageView)
         layout.addView(nameTextView)
         container.addView(layout)
-//バナナ
-        val nameTextView2 = TextView(this)
-        nameTextView2.text = banana.name
-        val layout2 = LinearLayout(this)
-        layout.orientation = LinearLayout.HORIZONTAL
 
-        val imageView2 = ImageView(this)
-        imageView2.setImageResource(banana.resId)
-        layout2.addView(imageView2)
-        layout2.addView(nameTextView2)
-        container.addView(layout2)
-//オレンジ
-        val nameTextView3 = TextView(this)
-        nameTextView3.text = orange.name
-        val layout3 = LinearLayout(this)
-        layout3.orientation = LinearLayout.HORIZONTAL
-        val imageView3 = ImageView(this)
-        imageView3.setImageResource(orange.resId)
-        layout3.addView(imageView3)
-        layout3.addView(nameTextView3)
-        container.addView(layout3)
-//いちご
-        val nameTextView4 = TextView(this)
-        nameTextView4.text = strawberry.name
-        val layout4 = LinearLayout(this)
-        layout4.orientation = LinearLayout.HORIZONTAL
-        val imageView4 = ImageView(this)
-        imageView4.setImageResource(strawberry.resId)
-        layout4.addView(imageView4)
-        layout4.addView(nameTextView4)
-        container.addView(layout4)
     }
+
 }
--- app/src/main/java/app/yamamoto/leaf/tango/Word.kt
+++ app/src/main/java/app/yamamoto/leaf/tango/Word.kt
@@ -3,7 +3,7 @@ package app.yamamoto.leaf.tango
 import android.os.Bundle
 import android.widget.TextView
 
-class Word (var resId: Int, var name : String)
+class Word (var resId : Int?, var name: String?)
 
 
 
